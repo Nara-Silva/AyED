93cf680b4c8059c755d3ac358dec381a46878082
Nara Santana <nsantanadasilva@frba.utn.edu.ar>
2023-06-01 22:02:51 -0300

    Update main.cpp

--- 01-CalcularPi/main.cpp
+++ 01-CalcularPi/main.cpp
@@ -1,1 +1,28 @@
 
+#include <iostream>
+#include <iomanip>  
+
+double calcular_pi(int min_iteraciones) {
+
+    double pi = 0.0;
+    double numerador = 1.0;
+    int multiplicador = 4;
+
+    for (int i = 1; i < min_iteraciones; i+=2) { 
+             
+        pi += multiplicador * (numerador / i);         
+        numerador *= -1;                      
+    }
+
+    return pi;
+}
+
+
+int main() {
+
+    int min_iteraciones = 2000000; 
+    double pi = calcular_pi(min_iteraciones);
+    std::cout << "Usando la Serie de Leibniz, el valor encontrado para pi (con 6 decimales) fue: " << std::setprecision(7) << std::fixed << pi << std::endl;
+
+    return 0;
+}
